825e84819b896fe83adec1d39fd998ff4010142d
serevina sherly maulida <101394654+serevinaash@users.noreply.github.com>
2024-08-13 09:59:33 +0700

    post

--- database/migrations/2024_08_05_085430_create_post_table.php
+++ database/migrations/2024_08_05_085430_create_post_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,15 +10,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->string('firstname', 100);
-            $table->string('lastname', 100)->nullable();
+            $table->string('title', 255);
+            $table->text('news_content');
+            $table->unsignedBigInteger('author');
             $table->timestamps();
             $table->softDeletes();
+
+            $table->foreign('author')->references('id')->on('users')->onDelete('cascade');  
         });
     }
 
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
